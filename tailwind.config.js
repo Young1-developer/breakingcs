a16ed15e00264300fedffab4fbcc7ef67fd5e374
usama <usamasaniabdullahi814@gmail.com>
2023-10-05 13:40:21 +0100

    adding fourth commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    container:{
+      center:true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
